85bc64761a37c74587273e7ea5f86382c0eeb410
Rui Vieira <ruidevieira@googlemail.com>
2021-10-26 16:27:07 +0100

    Add main cookiecutter file

---
+++ cookiecutter.el
@@ -0,0 +1,30 @@
+;;; cookiecutter.el --- Emacs cookiecutter utilities
+
+;; Copyright (C) 2021 Rui Vieira
+
+;; Author: Rui Vieira <ruidevieira@googlemail.com>
+;; Version: 0.1
+;; Keywords: python, projects
+;; URL: https://github.com/ruivieira/cookiecutter.el
+
+;;; Commentary:
+
+;; A package to manage cookiecutter projects.
+
+
+;;; Code:
+(defun cookiecutter--run-make-target (target)
+  (when (locate-dominating-file default-directory "Makefile")
+    (cd (locate-dominating-file default-directory "Makefile"))
+    (comint-send-string
+     (get-buffer-process (shell)) (format "make %s\n" target))))
+
+;;;###autoload
+(defun cookiecutter-ds-data ()
+  "Bind the main Makefile and create the dataset."
+  (interactive)
+(cookiecutter--run-make-target "data"))
+
+
+(provide 'cookiecutter)
+;;; cookiecutter.el ends here
